0c2ae88076496253c8d8b321a21383052fb6436b
Allan Holm Søndergaard <6605392@rts-365.dk>
2020-11-11 09:40:01 +0100

    alternate swipes added

--- javascript/app.js
+++ javascript/app.js
@@ -8,32 +8,30 @@ var limit = 50;
 var deleteMe = document.querySelector(".deleteMe");
 var wrapper = document.querySelector(".wrapper");
 
-
-
 wrapper.addEventListener("touchstart", function (event) {
     xStart = event.changedTouches[0].pageX;
     yStart = event.changedTouches[0].pageY;
-    deleteMe.classList.add("moved");
 });
 
 wrapper.addEventListener("touchend", function (event) {
     xEnd = event.changedTouches[0].pageX;
     yEnd = event.changedTouches[0].pageY;
 
-    if ((xEnd + tolerance) < xStart &&
-        yEnd < (yStart + limit) &&
-        yEnd > (yStart - limit)) {
-        deleteMe.classList.remove("moved");
-    }
-
     if ((xEnd - tolerance) > xStart &&
         yEnd < (yStart + limit) &&
         yEnd > (yStart - limit)) {
-        deleteMe.classList.add("moved");
+        deleteMe.classList.add("movedRight");
         setTimeout(function() {
             deleteMe.classList.add("deleted");
         }, 1000);
-    } else {
-        deleteMe.classList.remove("moved");
+    }
+
+    if ((xEnd + tolerance) < xStart &&
+        yEnd < (yStart + limit) &&
+        yEnd > (yStart - limit)) {
+        deleteMe.classList.add("movedLeft");
+        setTimeout(function() {
+            alert("I have been saved!")
+        }, 1000);
     }
 });
